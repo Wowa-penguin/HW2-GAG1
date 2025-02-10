34e0c525e877d70c3997647a56be41920a0a90d1
Einar S. <einstu.cis@gmail.com>
2025-02-10 08:55:19 +0000

    First Question

--- QUERIES.sql
+++ QUERIES.sql
@@ -8,6 +8,10 @@
 
 -- Explanation: 
 
+SELECT PL.name
+FROM plants PL
+    JOIN families FM ON FM.id = PL.familyid
+WHERE PL.name LIKE '%r%r%' AND FM.name = 'Ruellia';
 
 -- B. The most overfilled flowerbed is planted to 105% capacity. 
 --    What are the ID(s) of the flowerbed(s) with the most overfilled capacity?
